d4ba54e9ba6d2aad2cb6fe09ef2a71700a750ea0
Quentin <quentinbadie@gmail.com>
2025-08-20 14:33:28 +0200

    donor checkout and UI 109

--- src/pages/Okini List.hy57x.js
+++ src/pages/Okini List.hy57x.js
@@ -1,10 +1,23 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
-
 $w.onReady(function () {
-    // Write your JavaScript here
+    // This function runs when the switch is toggled.
+    $w('#switch1').onChange(() => {
+        // Check if the switch is turned on (checked).
+        if ($w('#switch1').checked) {
+            // If it's on, expand the section.
+            $w('#section18').expand();
+        } else {
+            // If it's off, collapse the section.
+            $w('#section18').collapse();
+        }
+    });
 
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    // This function runs when the button is clicked.
+    $w('#button18').onClick(() => {
+        // When the button is clicked, we do two things:
+        // 1. Turn the switch to the "on" position.
+        $w('#switch1').checked = true;
+        
+        // 2. Expand the section.
+        $w('#section18').expand();
+    });
 });
