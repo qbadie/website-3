9f086cff98a10ca13ef856ccd203082d39868c31
Quentin <quentinbadie@gmail.com>
2025-09-30 15:19:14 +0200

    UI tweak removed progress bar from admin

--- src/pages/Okini Admin.jxlu7.js
+++ src/pages/Okini Admin.jxlu7.js
@@ -9,39 +9,7 @@ const APPROVAL_FIELD_KEY = "approvedDonor";
 
 $w.onReady(function () {
     updateNewDonorCount();
-    setupRequestsRepeater();
 });
-
-
-/**
- * Sets up the repeater for operations to display the progress bar status.
- */
-function setupRequestsRepeater() {
-    // ACTION: Make sure this is the ID of your requests repeater.
-    $w("#requestsRepeater").onItemReady(($item, itemData, index) => {
-        const progressBar = $item("#progressBar1");
-        // FIX: Select the new text element you added.
-        const statusText = $item("#text128"); 
-        const status = itemData.operationType;
-
-        const statusSettings = {
-            "Requested": { value: 10 },
-            "Pledged": { value: 50 },
-            "PendingFulfillment": { value: 75 },
-            "Fulfilled": { value: 100 }
-        };
-
-        const currentStatus = statusSettings[status];
-
-        if (currentStatus) {
-            // 1. Set the progress bar's numerical value.
-            progressBar.value = currentStatus.value;
-            
-            // 2. Set the text of your new text element.
-            statusText.text = `${status}`;
-        }
-    });
-}
 
 
 /**
